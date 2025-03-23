f2a08c3437b4dbbadf317fc324b2863828149339
Noel <noel-fgc@proton.me>
2025-03-23 11:18:24 -0500

    create Program.cs and add CommandDB.json

---
+++ src/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace BBToolsCS {
+  public class BBScriptCommandData {
+    public string name { get; set; } = "a";
+    public string format { get; set; } = "I";
+    public int size { get; set; } = 4;
+    public bool hex { get; set; } = false;
+  }
+
+  class Program {
+    public static Dictionary<uint, BBScriptCommandData> fetchCommandDB() {
+      string dbPath = "CommandDB.json";
+      string jsonString = File.ReadAllText(dbPath);
+      
+      var commands = JsonSerializer.Deserialize<Dictionary<uint, BBScriptCommandData>>(jsonString);
+      if (commands is null) {
+        throw new NullReferenceException("Json loader returned null");
+      }
+      return commands;
+    }
+    static void Main(string[] args) {
+      var commandDB = fetchCommandDB();
+      Console.WriteLine(commandDB.Count + " Commands Loaded.");
+    }
+  }
+}

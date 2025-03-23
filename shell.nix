bd7ccb588478a569882ad9c05a7c0bd225a06240
Noel <noel-fgc@proton.me>
2025-03-23 11:17:56 -0500

    initial setup

--- shell.nix
+++ shell.nix
@@ -1,6 +1,12 @@
 { pkgs ? import <nixpkgs> {} }:
 
 pkgs.mkShell{
+  packages = [
+    pkgs.dotnet-sdk
+    pkgs.dotnet-runtime
+    pkgs.dotnet-aspnetcore
+    pkgs.dotnet-repl
+  ];
   shellHook = ''
     export SHELL=$(which zsh)
     nvim && exit

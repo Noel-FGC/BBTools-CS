749579a49a697210e763b963d0178543542c1697
Noel <noel-fgc@proton.me>
2025-03-24 06:01:42 -0500

    mildly more functional parser

--- src/BinParser.cs
+++ src/BinParser.cs
@@ -6,35 +6,37 @@ using PythonStructCS;
 namespace BBToolsCS {
   public class BinParser {
     private Stream stream;
+    private StreamWriter writer;
     private BinaryReader reader;
     private char Endian;
     private Dictionary<uint, BBScriptCommandData> CommandDB;
-    public BinParser(string path, Dictionary<uint, BBScriptCommandData> tmpCommandDB) {
+    public BinParser(string path, Dictionary<uint, BBScriptCommandData> tmpCommandDB, char endian = '<') {
       stream = File.Open(path, FileMode.Open);
+      writer = new StreamWriter(Path.ChangeExtension(path, ".cs"));
       reader = new BinaryReader(stream);
-      Endian = '<';
+      Endian = endian;
       CommandDB = tmpCommandDB;
       parseBBScriptRoutine();
     }
     
     void parseBBScriptRoutine() {
-      var FunctionCount = reader.ReadUInt32();
+      uint FunctionCount = (uint) PyStruct.unpack(Endian + "I", reader.ReadBytes(4))[0];
       stream.Seek(FunctionCount * 0x24, SeekOrigin.Current);
       Console.WriteLine(FunctionCount);
       while (stream.Position < stream.Length) {
-        uint commandID = reader.ReadUInt32();
+        uint commandID = (uint) PyStruct.unpack(Endian + "I", reader.ReadBytes(4))[0];
         string format = CommandDB[commandID].format;
-        Console.Write(CommandDB[commandID].name);
-        byte[] tmp = reader.ReadBytes(PyStruct.calcSize(format));
-        List<object> commandData = PyStruct.unpack(format, tmp);
-        Console.Write('(');
+        writer.Write(CommandDB[commandID].name);
+        List<object> commandData = PyStruct.unpack(Endian + format, reader.ReadBytes(PyStruct.calcSize(format)));
+        writer.Write('(');
         foreach (object val in commandData) {
-          Console.Write(val);
+          writer.Write(" " + val.GetType() + ": " + val);
+          /*writer.Write(val);*/
           if (val != commandData.Last()) {
-            Console.Write(", ");
+            writer.Write(", ");
           }
         }
-        Console.Write(")\n");
+        writer.Write(");\n");
       }
     }
   }
